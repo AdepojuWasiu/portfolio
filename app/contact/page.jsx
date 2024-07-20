b11761ef60cb8d80c3a38340d85d98032f0a85b5
AdepojuWasiu <adepojuwasiu031@gmail.com>
2024-07-21 00:53:34 +0100

    fix typo

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -74,7 +74,7 @@ const Contact = () => {
     >
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
-        className='flex-[0.75] bg-black-100 p-8 rounded-2xl pl-[10px]'
+        className='flex-[0.75] bg-black-100 p-8 rounded-2xl pr-[10px]'
       >
         <p className={styles.sectionSubText}>Get in touch</p>
 
