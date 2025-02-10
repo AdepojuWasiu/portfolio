1b71e78bc4b78591df1a76436e840e6a15c72d44
AdepojuWasiu <adepojuwasiu031@gmail.com>
2025-02-10 07:31:19 +0100

    Project update

--- constant.js
+++ constant.js
@@ -1,61 +1,83 @@
 
 const projects = [
     { id: 1,
+        image: '/assets/project-pic-11.png',
+        title: 'Web development',
+        name: 'Jobs Application Server',
+        description : 'A job application server built with node js, express server, PogresSQL(supabase) and tested with POSTMAN. ',
+        link: 'https://jobs-red-ten.vercel.app/'
+    },
+       { id: 2,
+        image: '/assets/project-pic-10.png',
+        title: 'Web development',
+        name: 'Dashboard',
+        description : 'A dashboard UI built with React and styled with TailwindCSS. ',
+        link: 'https://dashboard-lime-seven.vercel.app/'
+     },
+     { id: 3,
+        image: '/assets/project-pic-9.png',
+        title: 'Web development',
+        name: 'React Redux Staff Management',
+        description : 'A staff management apps built with React, Redux and styled with TailwindCSS.',
+        link: 'https://react-redux-crud-zeta.vercel.app/'
+       },
+    { id: 4,
         image: '/assets/project-pic-8.jpg',
         title: 'Web development',
         name: 'NONAGON',
         description : 'A full-stack Telegram web application built with Next js, styled with Tailwind CSS, Mongodb as database, RestFul API and deployed on vercel.',
         link: 'https://t.me/Nonagonbot/nonagon?startapp=6103232944'
        },
-    { id: 2,
+    { id: 5,
      image: '/assets/project-pic-1.png',
      title: 'Web development',
      name: 'Wasquo Coin',
      description : 'A full-stack coin minning web application built with Next js, styled with Tailwind CSS, using OAuth authentication, Mongodb as database, RestFul API and deployed on vercel.',
      link: 'https://wasquo-coin.vercel.app/'
     },
-    {   id:3,
+    {   id:6,
         image: '/assets/project-pic-2.png',
         title: 'Web development',
         name: 'My Portfolio',
         description : 'A 3D web application built with Next js, Three js, styled with Tailwind CSS, using Email js API and deployed on vercel.',
         link: '/'
     },
-    {  id:4,
+    {  id:11,
+        image: '/assets/project-pic-7.png',
+        title: 'Web development',
+        name: 'My Second Portfolio',
+        description : ' My second portfolio built with HTML, CSS and Javascript. Deployed using vercel.',
+        link: 'https://bincon-portfolio.vercel.app/'
+    },
+    {  id:7,
         image: '/assets/project-pic-3.png',
         title: 'Machine Learning',
         name: 'Credit Card Fraud Detection',
         description : ' A credit card fraud detection project built with python, scikit-learn, pandas, numpy, matplotlib, seaborn and jupyter notebook. ',
         link: 'https://github.com/AdepojuWasiu/Credit-Card'
     },
-    {   id:5,
+    {   id:8,
         image: '/assets/project-pic-4.png',
         title: 'Web development',
         name: 'Simple Calculator',
         description : 'A simple calculator designed for carrying out basic mathematical operations. Built with HTML, CSS and javascript. Deployed using github pages.',
         link: 'https://adepojuwasiu.github.io/calculator/'
     },
-    {   id:6,
+    {   id:9,
         image: '/assets/project-pic-5.png',
         title: 'Data Analysis',
         name: 'Customer Data Analysis',
         description : 'A shop customer data analysis project built with python, pandas, numpy, matplotlib, seaborn and jupyter notebook.',
         link: 'https://github.com/AdepojuWasiu/Customer-Project'
     },
-    {   id:7,
+    {   id:10,
         image: '/assets/project-pic-6.png',
         title: 'Machine Learning',
         name: 'Car Price Prediction',
         description : 'A car price prediction project built with python, scikit-learn, pandas, numpy, matplotlib, seaborn and jupyter notebook. ',
         link: 'https://github.com/AdepojuWasiu/Car-Price'
     },
-    {  id:8,
-        image: '/assets/project-pic-7.png',
-        title: 'Web development',
-        name: 'My Old Portfolio',
-        description : ' My old portfolio built with HTML, CSS and Javascript. Deployed using github page.',
-        link: 'https://adepojuwasiu.github.io/my-portfolio/'
-    }
+  
 ]
 
 export default projects;
